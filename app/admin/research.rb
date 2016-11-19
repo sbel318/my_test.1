834259baf7c94a31a64c35da4edf42e44b852ac2
Prelang Builder <builder@prelang.com>
2016-11-19 19:49:17 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Research' to allow for create, update

--- app/admin/research.rb
+++ app/admin/research.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Research do
+  permit_params :stock, :summary
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
